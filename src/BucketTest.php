93d50a0e5b53b423a5c2f39a115f929216f88022
Daniel Fekete <daniel.fekete@voov.hu>
2016-08-15 15:27:43 +0200

    Tests init

---
+++ src/BucketTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2016, VOOV LLC.
+ * All rights reserved.
+ * Written by Daniel Fekete
+ * daniel.fekete@voov.hu
+ */
+
+namespace danfekete\Sequential;
+
+
+class BucketTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetData()
+    {
+        $bucket = new Bucket(1, 'a', 2);
+        $this->assertEquals([1, 'a', 2], $bucket->getData());
+    }
+
+    public function testKey()
+    {
+        $bucket = new Bucket(1, 'a', 2);
+        $this->assertEquals('1-a-2', $bucket->key());
+    }
+
+
+}
---
+++ tests/SQLiteTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Copyright (c) 2016, VOOV LLC.
+ * All rights reserved.
+ * Written by Daniel Fekete
+ * daniel.fekete@voov.hu
+ */
+class SQLiteTest extends PHPUnit_Framework_TestCase
+{
+    
+}
